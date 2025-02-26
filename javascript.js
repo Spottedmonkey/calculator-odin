5dd55e709c637e0e687ee06b2caf131708c1027d
Spottedmonkey <186652520+Spottedmonkey@users.noreply.github.com>
2025-02-25 20:03:58 -0600

    Fixed: function to compute equations

--- javascript.js
+++ javascript.js
@@ -18,149 +18,139 @@ const periodBtn = document.querySelector("#period");
 const equalBtn = document.querySelector("#equal-btn");
 const deleteBtn = document.querySelector("#delete-btn");
 
-let add = (number1, number2) => number1 + number2;
+function add(number1, number2) {
+  number1 = Number(number1);
+  number2 = Number(number2);
+  let calculatedNumber = number1 + number2;
+  return Number(calculatedNumber.toFixed(2));
+}
 
-let subtract = (number1, number2) => number1 - number2;
+function subtract(number1, number2) {
+  number1 = Number(number1);
+  number2 = Number(number2);
+  let calculatedNumber = number1 - number2;
+  return Number(calculatedNumber.toFixed(2));
+}
 
-let multiply = (number1, number2) => number1 * number2;
+function multiply(number1, number2) {
+  number1 = Number(number1);
+  number2 = Number(number2);
+  let calculatedNumber = number1 * number2;
+  return Number(calculatedNumber.toFixed(2));
+}
+function divide(number1, number2) {
+  number1 = Number(number1);
+  number2 = Number(number2);
+  let calculatedNumber = number1 / number2;
+  return Number(calculatedNumber.toFixed(2));
+}
 
-let divide = (number1, number2) => number1 / number2;
-
-let equation = "";
+let num1 = "";
+let operand = "";
+let num2 = "";
 
 function storeEquation(character) {
-  equation += character;
-  header.textContent = equation;
-}
-
-function filtersArrOperands(arr) {
-  let splitArr = arr.split("");
-  let plusOperands = splitArr.filter((item) => item === "+").join("");
-  let minusOperand = splitArr.filter((item) => item === "-").join("");
-  let multiplyOperand = splitArr.filter((item) => item === "*").join("");
-  let divideOperand = splitArr.filter((item) => item === "/").join("");
-  let totalOperands =
-    plusOperands + minusOperand + multiplyOperand + divideOperand;
-
-  return totalOperands.split("").length;
-}
-
-function calculateArray(arr) {
-  let newArr = arr.split(" ");
-
-  for (let i = 0; i < filtersArrOperands(arr); i++) {
-    let num1 = Number(newArr[0]);
-    let operand = newArr[1];
-    let num2 = Number(newArr[2]);
-    let calculatedSplit = operate(num1, operand, num2).join("");
-    console.log(newArr);
-    console.log(calculatedSplit);
-    newArr.splice(0, 3, calculatedSplit);
-    console.log(newArr);
+  if (operand.length < 1) {
+    num1 += character;
+    header.textContent = num1;
+  } else {
+    num2 += character;
+    header.textContent = num2;
   }
+}
 
-  return newArr;
+function storeOperand(character) {
+  operand = "";
+  operand += character;
 }
 
 function operate(num1, operand, num2) {
-  return operand === "+"
-    ? (equation = [add(num1, num2)])
-    : operand === "-"
-    ? (equation = [subtract(num1, num2)])
-    : operand === "*"
-    ? (equation = [multiply(num1, num2)])
-    : operand === "/"
-    ? (equation = [divide(num1, num2)])
-    : "ERROR";
+  if (operand === "+") {
+    return add(num1, num2);
+  } else if (operand === "-") {
+    return subtract(num1, num2);
+  } else if (operand === "*") {
+    return multiply(num1, num2);
+  } else if (operand === "/") {
+    return divide(num1, num2);
+  } else {
+    ("ERROR");
+  }
 }
 
 clearBtn.addEventListener("click", () => {
-  equation = "";
-  header.textContent = equation;
-  console.log(equation);
+  num1 = "";
+  operand = "";
+  num2 = "";
+  header.textContent = "";
 });
 
 divideBtn.addEventListener("click", () => {
-  storeEquation(" / ");
+  storeOperand("/");
 });
 
 sevenBtn.addEventListener("click", () => {
   storeEquation(7);
-  console.log(equation);
 });
 
 eightBtn.addEventListener("click", () => {
   storeEquation(8);
-  console.log(equation);
 });
 
 nineBtn.addEventListener("click", () => {
   storeEquation(9);
-  console.log(equation);
 });
 
 multiplyBtn.addEventListener("click", () => {
-  storeEquation(" * ");
-  console.log(equation);
+  storeOperand("*");
 });
 
 fourBtn.addEventListener("click", () => {
   storeEquation(4);
-  console.log(equation);
 });
 
 fiveBtn.addEventListener("click", () => {
   storeEquation(5);
-  console.log(equation);
 });
 
 sixBtn.addEventListener("click", () => {
   storeEquation(6);
-  console.log(equation);
 });
 
 minusBtn.addEventListener("click", () => {
-  storeEquation(" - ");
-  console.log(equation);
+  storeOperand("-");
 });
 
 oneBtn.addEventListener("click", () => {
   storeEquation(1);
-  console.log(equation);
 });
 
 twoBtn.addEventListener("click", () => {
   storeEquation(2);
-  console.log(equation);
 });
 
 threeBtn.addEventListener("click", () => {
   storeEquation(3);
-  console.log(equation);
 });
 
 plusBtn.addEventListener("click", () => {
-  storeEquation(" + ");
-  console.log(equation);
+  storeOperand("+");
 });
 
 zeroBtn.addEventListener("click", () => {
   storeEquation(0);
-  console.log(equation);
 });
 
 periodBtn.addEventListener("click", () => {
-  storeEquation(" . ");
-  console.log(equation);
+  storeEquation(".");
 });
 
 equalBtn.addEventListener("click", () => {
-  calculateArray(equation);
-  header.textContent = equation;
+  let result = operate(num1, operand, num2);
+  num1 = String(result);
+  header.textContent = result;
+  operand = "";
+  num2 = "";
 });
 
-deleteBtn.addEventListener("click", () => {
-  equation = equation.replace(equation.at(-1), "");
-  header.textContent = equation;
-  console.log(equation);
-});
+deleteBtn.addEventListener("click", () => {});
